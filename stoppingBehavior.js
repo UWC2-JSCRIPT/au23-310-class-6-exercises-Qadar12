54c7f685ba040a5f45b222f15325b1e2a9aecc55
qadar omar <omarqadar3@gmail.com>
2023-11-16 20:22:15 -0800

    completed with jq event handler

--- stoppingBehavior.js
+++ stoppingBehavior.js
@@ -1,10 +1,22 @@
 // Do not change
-document.getElementById('cat').addEventListener('click', () => {
-  alert('meow!');
+
+$(document).ready(function () {
+  document.getElementById("cat").addEventListener("click", () => {
+    alert("meow!");
+  });
+
+  // When clicked, "More info" link should alert "Here's some info"
+  // instead of going to a new webpage
+
+  $("#more-info").on("click", function (event) {
+    event.preventDefault();
+    alert("More info");
+  });
+
+  // When the bark button is clicked, should alert "Bow wow!"
+  // Should *not* alert "meow"
+  $(".dog").on("click", function (event) {
+    event.stopPropagation();
+    alert("bark");
+  });
 });
-
-// When clicked, "More info" link should alert "Here's some info"
-// instead of going to a new webpage
-
-// When the bark button is clicked, should alert "Bow wow!"
-// Should *not* alert "meow"
