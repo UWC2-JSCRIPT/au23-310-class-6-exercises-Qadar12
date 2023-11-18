1b1a8df7a134b66a193bf45637ea87a5fa018ef9
qadar omar <omarqadar3@gmail.com>
2023-11-18 10:28:43 -0800

    domCrud and car proto

--- carProto.js
+++ carProto.js
@@ -4,12 +4,63 @@
  * @param {String} model
  */
 
+/*
 //  Create an instance, accelerate twice, brake once, and console log the instance.toString()
+class Car {
+  constructor(model) {
+    this.currentSpeed = 0;
+    this.model = model;
+  }
 
+  accelerate() {
+    return (this.currentSpeed += 1);
+  }
+
+  brake() {
+    return (this.currentSpeed -= 1);
+  }
+
+  toString() {
+    return `${this.model} is going ${this.currentSpeed} mph`;
+  }
+}
 /**
  * ElectricCar class
  * @constructor
  * @param {String} model
- */
+ 
 
 //  Create an instance, accelerate twice, brake once, and console log the instance.toString()
+const honda = new Car("honda");
+
+honda.accelerate();
+honda.accelerate();
+honda.brake();
+honda.toString();
+
+*/
+class Car {
+  constructor(make, model, maxSpeed) {
+    this.make = make;
+    this.model = model;
+    this.maxSpeed = maxSpeed;
+  }
+  honk() {
+    console.log("Beep");
+  }
+  toString() {
+    return `${this.make} ${this.model} with a max speed of ${this.maxSpeed}`;
+  }
+}
+class Porsche extends Car {
+  constructor(model, maxSpeed) {
+    super("Porsche", model, maxSpeed);
+    this.turboCharged = true;
+  }
+}
+const nineEleven = new Porsche("911", 160);
+console.log(nineEleven.toString());
+nineEleven.honk();
+
+// Panamera or 918 Spyder
+
--- jQueryDomCrud.js
+++ jQueryDomCrud.js
@@ -1,15 +1,35 @@
-// Create a new <a> element containing the text "Buy Now!"
-// with an id of "cta" after the last <p>
+// Wrap your JS code in the jQuery document.ready method
+$(document).ready(function () {
+  // Access (read) the data-color attribute of the <img>,
+  // log to the console
 
-// Access (read) the data-color attribute of the <img>,
-// log to the console
+  let img = $("img").data("color");
+  console.log(img);
 
-// Update the third <li> item ("Turbocharged"),
-// set the class name to "highlight"
+  // Update the third <li> item ("Turbocharged"),
+  // set the class name to "highlight" index is eq
 
-// Remove (delete) the last paragraph
-// (starts with "Available for purchase now…")
+  $("li:eq(2)").addClass("highlight");
 
-// Create a listener on the "Buy Now!" link that responds to a click event.
-// When clicked, the the "Buy Now!" link should be removed
-// and replaced with text that says "Added to cart"
+  // Create a new <a> element containing the text "Buy Now!"
+  // with an id of "cta" after the last <p>
+
+  let newA = $('<a href="#">');
+  newA.text("Buy Now");
+  newA.attr("id", "cta");
+  $("main").append(newA);
+
+  // Remove (delete) the last paragraph
+  // (starts with "Available for purchase now…")
+
+  // deleting the content but not the element <p>
+  $("p").empty();
+
+  // Create a listener on the "Buy Now!" link that responds to a click event.
+  // When clicked, the the "Buy Now!" link should be removed
+  // and replaced with text that says "Added to cart"
+
+  newA.on("click", function () {
+    $(this).html("Added to Cart");
+  });
+});
