1d648ed2e1d8b78737ab7e3bdd6470d272170665
qadar omar <omarqadar3@gmail.com>
2023-11-19 09:50:17 -0800

    ToDoList w/ jq

--- jQueryToDoList.js
+++ jQueryToDoList.js
@@ -1,19 +1,52 @@
-/**
- * Toggles "done" class on <li> element
- */
+$(document).ready(function () {
+  /**
+   * Toggles "done" class on <li> element.
+   * event-delegation: start at parent element to child to toggle
+   */
+  $("ul").on("click", "li", function () {
+    $(this).toggleClass("done");
+  });
 
-/**
- * Delete element when delete link clicked
- */
+  /**
+   * Delete element when delete link clicked.
+   * event-delegation: start at parent element to child to delete class and fadeOut
+   */
+  $("ul").on("click", ".delete", function (event) {
+    event.stopPropagation();
 
-/**
- * Adds new list item to <ul>
- */
-const addListItem = function(e) {
-  e.preventDefault();
-  const text = $('input').val();
+    let rm = $(this).closest("li");
+    $(this).fadeOut("slow", function () {
+      rm.remove();
+    });
+  });
 
-  // rest here...
-};
+  /**
+   * Adds new list item to <ul>
+   * find parent,create li, add&append values and append to parent
+   */
+  const addListItem = function (e) {
+    const text = $("input").val();
+    let ul = $("ul");
+    let newItem = $("<li>");
 
-// add listener for add
+    newItem.append(
+      $("<span>").text(text),
+      $("<a>").addClass("delete").text("Delete")
+    );
+
+    ul.append(newItem);
+
+    $("input").val("");
+  };
+
+  // add listener for add
+
+  $(".add-item").on("click", addListItem);
+});
+
+/*
+<li>
+ <span>Homework</span>
+ <a class="delete">Delete</a>
+</li>
+*/
